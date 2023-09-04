fb14641cd3c2a071d4bd13325c1196bbb3dac975
KseniaShepherd <colorleafs8@gmail.com>
2023-09-04 14:19:48 +0200

    fixed postcss.config

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 module.exports = {
     plugins: [
-      require('tailwindcss'),
-      require('autoprefixer'),
-    ],
+        tailwindcss,
+        autoprefixer
+      ],
   }
